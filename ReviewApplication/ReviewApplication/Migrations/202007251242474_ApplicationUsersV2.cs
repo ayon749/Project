5590a44a518307375c8e004167b7afc6597cb7e5
Mushfiqur Rahman <mushfiqurrahman749@gmail.com>
2020-07-25 19:27:42 +0600

    registration of new users done

---
+++ ReviewApplication/ReviewApplication/Migrations/202007251242474_ApplicationUsersV2.cs
@@ -0,0 +1,20 @@
+﻿namespace ReviewApplication.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class ApplicationUsersV2 : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.AspNetUsers", "Name", c => c.String());
+            AddColumn("dbo.AspNetUsers", "GenderId", c => c.Int(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.AspNetUsers", "GenderId");
+            DropColumn("dbo.AspNetUsers", "Name");
+        }
+    }
+}
