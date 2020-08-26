2757a68e58a2732f855d73c524f510c9fb9968de
Mushfiqur Rahman <mushfiqurrahman749@gmail.com>
2020-08-26 16:17:12 +0600

    Project created and user registration api done

---
+++ ReviewVaiApp/ReviewVaiApp/Migrations/202008261013494_IdentityUserV4.cs
@@ -0,0 +1,18 @@
+﻿namespace ReviewVaiApp.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class IdentityUserV4 : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.AspNetUsers", "CreatedAt", c => c.DateTime());
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.AspNetUsers", "CreatedAt", c => c.String());
+        }
+    }
+}
