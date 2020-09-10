918611318b109e50a59628fe283c8b748a32cc1d
Mushfiqur Rahman <mushfiqurrahman749@gmail.com>
2020-09-10 22:36:48 +0600

    tag,restaurant,item api created

---
+++ ReviewVaiApp/ReviewVaiApp/Controllers/ItemController.cs
@@ -0,0 +1,26 @@
+﻿using ReviewVaiApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ReviewVaiApp.Controllers
+{
+    public class ItemController : ApiController
+    {
+		private ApplicationDbContext db = new ApplicationDbContext();
+
+		public IHttpActionResult GetItems()
+		{
+			var items = db.Items.ToList();
+			if (items == null)
+			{
+				return BadRequest();
+			}
+			return Ok(items);
+		}
+
+	}
+}
---
+++ ReviewVaiApp/ReviewVaiApp/Migrations/202009091753117_CommentReactionv1.cs
@@ -0,0 +1,37 @@
+﻿namespace ReviewVaiApp.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class CommentReactionv1 : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.CommentReaction",
+                c => new
+                    {
+                        Id = c.Long(nullable: false, identity: true),
+                        PostCommentId = c.Long(nullable: false),
+                        ApplicationUserId = c.String(maxLength: 128),
+                        IsLiked = c.Int(nullable: false),
+                        IsHelpfull = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.AspNetUsers", t => t.ApplicationUserId)
+                .ForeignKey("dbo.PostComment", t => t.PostCommentId, cascadeDelete: true)
+                .Index(t => t.PostCommentId)
+                .Index(t => t.ApplicationUserId);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.CommentReaction", "PostCommentId", "dbo.PostComment");
+            DropForeignKey("dbo.CommentReaction", "ApplicationUserId", "dbo.AspNetUsers");
+            DropIndex("dbo.CommentReaction", new[] { "ApplicationUserId" });
+            DropIndex("dbo.CommentReaction", new[] { "PostCommentId" });
+            DropTable("dbo.CommentReaction");
+        }
+    }
+}
