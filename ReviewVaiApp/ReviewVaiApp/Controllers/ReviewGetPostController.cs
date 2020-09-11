aad1091a4e8214fefdba7326d832a29eb91eb8a9
Himu <Himu@DESKTOP-KSVV9J9>
2020-09-11 06:45:24 +0600

    Did Nothing.

---
+++ ReviewVaiApp/ReviewVaiApp/Controllers/ReviewGetPostController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ReviewVaiApp.Controllers
+{
+    public class ReviewGetPostController : Controller
+    {
+        // GET: ReviewGetPost
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult GetReviews()
+        {
+            //var token = sessionStorage.getItem('access_token');
+            return View();
+        }
+    }
+}
