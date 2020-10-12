626641a93c5c7bcdebd7d469bfc546e8f2104891
himuahmed <mohsi.ahamed@gmail.com>
2020-10-12 20:06:02 +0600

    #Fixed logout bug

--- ReviewVaiApp/ReviewVaiApp/App_Start/RouteConfig.cs
+++ ReviewVaiApp/ReviewVaiApp/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace ReviewVaiApp
 			routes.MapRoute(
 				name: "Default",
 				url: "{controller}/{action}/{id}",
-				defaults: new { controller = "home", action = "index", id = UrlParameter.Optional }
+				defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
 			);
 		}
 	}
